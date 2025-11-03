700c5bcd44d7f2b3cacc165d4e2fd440eb9daf3a
Diogo Krüger <diogoks2005@gmail.com>
2025-11-02 22:18:42 -0300

    Adicionando janela

---
+++ src/TrabalhoPS/TrabalhoPS.java
@@ -0,0 +1,13 @@
+package TrabalhoPS;
+
+import javax.swing.JFrame;
+
+public class TrabalhoPS extends JFrame{
+    void main(){
+        setSize(750, 550);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
+    }
+}
