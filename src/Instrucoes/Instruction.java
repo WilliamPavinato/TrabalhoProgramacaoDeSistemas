cb753cb0fc3fd671545e31f75fdff37a47126c7d
GuEstivalet <guestivalet@hotmail.com>
2025-11-26 17:05:11 -0300

    feat: funcoes em Instruction

--- src/Instrucoes/Instruction.java
+++ src/Instrucoes/Instruction.java
@@ -48,4 +48,67 @@ public abstract class Instruction {
     {
 
     }
+
+     public int calcularTA(Registradores registradores, Memoria memoria) 
+     {
+        int x = 0;           // endereçamento indexado que é somado com o TA caso o flag x = 1
+        int m = 0;           // onde o operando vai será armazenado
+        int tamanhom = 0;
+
+
+        int pc = registradores.getValorPC();
+        
+        setFlags(memoria.getBytes(pc, 2));
+
+        if(!(flags.get("i") || flags.get("n"))) 
+        { 
+            m = getDispbpe(memoria.getBytes(pc, 3)); // tipo de instrução 3, com disp sendo os ultimos 15 bits da instrução
+            tamanhom = 15;
+        } 
+
+        else if (flags.get("e")) 
+        {
+            m = getAddr(memoria.getBytes(pc, 4)); // tipo de instrução 4, com addr sendo os ultimos 20 bits da instrução
+            tamanhom = 20;
+        } 
+
+        else
+        { 
+            m = getDisp(memoria.getBytes(pc, 3)); // tipo de instrução 3, com disp sendo os ultimos 12 bits da instrução
+            tamanhom = 12;
+        }
+
+
+        registradores.incrementarPC(getFormato(memoria.getBytes(registradores.getValorPC(), 2)));
+        
+        if(flags.get("b")) 
+        { 
+            base += registradores.getRegistradorPorNome("B").getValorIntSigned();
+        } 
+        
+        else if (flags.get("p")) 
+        {
+            base += registradores.getValorPC();
+            m = (int) (m << (32 - tamanhom)) >> (32 - tamanhom); // extende o sinal para ter interpretado como um inteiro com sinal
+        }
+
+        // ambos são 0 caso não seja modo de endereçamento relativo a base (endereçamento direto, normalmente sendo formato de instrução 4)
+    
+        if(flags.get("x")) 
+        { 
+            x = registradores.getRegistradorPorNome("X").getValorIntSigned();
+        }
+
+
+        if(flags.get("i") && !flags.get("n")) 
+        {            
+            return m + base;                                        // endereçamento indexado não pode ser usado com endereçamento imediato, então não soma X
+        } 
+        
+        else if (flags.get("n") && !flags.get("i")) 
+        {   
+            return m + base;                                      
+        }
+
+        return m+base+x;                                         
 }
