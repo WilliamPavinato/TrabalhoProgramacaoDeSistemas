282ab571aad4a0574310bfed98cb8065a1c24006
kbw1nter <kanandabw@gmail.com>
2025-11-27 20:43:17 -0300

    Atualizado LDS (Dnv)

--- src/Instrucoes/LDS.java
+++ src/Instrucoes/LDS.java
@@ -4,30 +4,22 @@ import Executor.Memoria;
 import Executor.Registradores;
 
 public class LDS extends Instruction {
-
     public LDS() {
-        //passando os 4 argumentos que a classe instruction pede
         super("LDS", (byte)0x6C, "3/4", 3);
     }
 
     @Override
     public void executar(Memoria memoria, Registradores registradores) {
-        // O Executor já fez o incremento do PC antes de chamar o executar.
-        // Então o PC atual aponta para o OPERANDO (o endereço).
         int pc = registradores.getValorPC();
 
-        //Usando 'getWord' (ou getByte) porque 'getPosicaoMemoria' não existe na sua classe Memoria
-        // Assumindo que o endereço é um número salvo na memória.
-        int enderecoMem = memoria.getWord(pc);
+        //lê apenas 1 byte para o endereço (pois o montador gerou 1 linha)
+        int enderecoOperando = memoria.getByte(pc) & 0xFF; // & 0xFF converte byte negativo para int positivo
 
-        // passando '1' para incrementarPC, pois ele exige um argumento int
-        // Avançamos 1 posição porque acabamos de ler o operando.
         registradores.incrementarPC(1);
 
-        // busca o valor real na memória (no endereço que lemos acima)
-        int valorMem = memoria.getWord(enderecoMem);
+        //busca o valor na memória (GetWord lê 3 bytes, ideal para variáveis inteiras)
+        int valor = memoria.getWord(enderecoOperando);
 
-        // salva no registrador S
-        registradores.getRegistradorPorNome("S").setValorInt(valorMem);
+        registradores.getRegistradorPorNome("S").setValorInt(valor);
     }
 }
